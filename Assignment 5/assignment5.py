a785b61603fe4c587d2ebcd6a69fc25345e8d248
kalyanrohan <sri.rohan@binus.ac.id>
2020-10-27 22:52:32 +0700

    update

--- Assignment 5/assignment5.py
+++ Assignment 5/assignment5.py
@@ -1,9 +1,22 @@
 from collections import defaultdict
 dict1=defaultdict(int)
-with open ('sampletext.txt','r') as f:
-    txt=f.read()
-    txt.split(" ",",",".","?","!",":",)
-print(txt)
+f=open ('sampletext.txt','r')
+txt=f.read().split()
+new_txt=[]
+for word in txt:
+    new_txt.append(word.strip('!.:;,?-/_$%()'))
+
+for word in new_txt:
+    dict1[word]+=1
+
+hapaxes=[]
+
+for word,count in dict1.items():
+    if count==1:
+        hapaxes.append(word)
+print(hapaxes)
+print(len(hapaxes))
+print(len(new_txt))
 
 
 
