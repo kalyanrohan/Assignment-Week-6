c782820a937c56aa4b9ab79c74d42a5e7d8a2d8a
kalyanrohan <sri.rohan@binus.ac.id>
2020-10-28 00:04:13 +0700

    Update assignment3.py

--- Assignment 3/assignment3.py
+++ Assignment 3/assignment3.py
@@ -14,3 +14,12 @@ prices = {
 groceries=["banana","orange","apple"]
 def compute_bill(food):
     total=0
+    for item in food:
+        if stock[item]>0:
+            total_price_of_item=prices[item]*stock[item]
+            total+=total_price_of_item
+            stock[item]-=1 
+    return total
+
+print(f'Your total is: ${compute_bill(groceries)}')
+print(stock['banana'])
