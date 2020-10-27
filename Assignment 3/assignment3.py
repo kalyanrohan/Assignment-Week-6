4a38f419b95c30fc1d9a6502ed31a742a3b7c8ed
kalyanrohan <sri.rohan@binus.ac.id>
2020-10-27 19:19:00 +0700

    Update assignment3.py

---
+++ Assignment 3/assignment3.py
@@ -0,0 +1,16 @@
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+groceries=["banana","orange","apple"]
+def compute_bill(food):
+    total=0
