4c31ed2e3b9db1b52f0c463024e4960b5e1fb3d9
kalyanrohan <sri.rohan@binus.ac.id>
2020-10-29 01:31:11 +0700

    Update assignment9.py

--- Assignment 9/assignment9.py
+++ Assignment 9/assignment9.py
@@ -2,15 +2,15 @@ f=open('sampletext1.txt','r',encoding='utf-8')
 txt=f.read().split()
 exceptions=['Mr.','Jr.','Ms','Mrs','Dr.']
 i=0
+new_text=''
 
-
-for word in txt:
-    new_txt=txt[i]
-    if "?" not in word or "!" not in word:
-        new_txt.join(word)
-        i+=1
+new_text.join(exceptions)
+# for word in txt:
+#     if '?' not in word or '!' not in word:
+#         new_text.join(word)
+    
         
-print(new_txt)
+print(new_text)
 
 
 
