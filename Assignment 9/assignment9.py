7646aa5279a9bd8a1c3995d495cc5f69f1879692
kalyanrohan <sri.rohan@binus.ac.id>
2020-10-29 11:20:28 +0700

    final

--- Assignment 9/assignment9.py
+++ Assignment 9/assignment9.py
@@ -1,16 +1,20 @@
 f=open('sampletext1.txt','r',encoding='utf-8')
 txt=f.read().split()
-exceptions=['Mr.','Jr.','Ms','Mrs','Dr.']
-i=0
+exceptions=['Mr.','Jr.','Ms.','Mrs.','Dr.']
+print(txt)
 new_text=''
+for word in txt:
+    if '?' not in word and '!' not in word:
+        new_text+=(word + " ")
+        if '.' in word.strip('.') and word not in exceptions:
+            pass
+        elif '.' in word[-1] and word not in exceptions:
+            new_text+="\n"
+    else:
+        new_text+=(word + "\n")
 
-new_text.join(exceptions)
-# for word in txt:
-#     if '?' not in word or '!' not in word:
-#         new_text.join(word)
-    
-        
-print(new_text)
+print(new_text)   
+
 
 
 
